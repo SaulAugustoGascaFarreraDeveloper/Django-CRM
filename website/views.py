4ddd4f344e711fc13ba7ded94673c1e82006723a
Saul Augusto Gasca Farrera <sgfarreradev@gmail.com>
2023-04-18 15:17:28 -0600

    Update DCRM data

--- website/views.py
+++ website/views.py
@@ -89,3 +89,15 @@ def add_record(request):
     else:
         messages.success(request,"You must be logged in to do that...")
         return redirect('home')
+    
+
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)   
+        form = AddRecordForm(request.POST or None,instance=customer_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Record has been updated...")
+            return redirect('home')
+        return render(request,'update_record.html',{'form':form})
+    
